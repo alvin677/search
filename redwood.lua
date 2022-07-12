0476ad9b3dc3de391471013b315d6fa978b26649
Alvin Lodel <73744009+alvin677@users.noreply.github.com>
2022-07-12 03:35:41 +0200

    Add files via upload

---
+++ redwood.lua
@@ -0,0 +1,15 @@
+--[[
+Redwood Prison GUI Created By Nameless & Jalos34
+ 
+ / Join Our Discord For Vip Info: https://discordapp.com/invite/JgMREGp
+ / Please Read The TOS before using: https://pastebin.com/rGfVUegA
+ / Accounts That Are Under 5 Days Old Cannot Use The GUI
+]]--
+ 
+--// GUI & Settings (You Can Edit)
+_G.WALKSPEED = 100
+_G.JUMPPOWER = 100
+_G.GUICLICKID = "903267862"
+_G.TOGGLE = Enum.KeyCode.F
+_G.toolsonspawn = {""}
+loadstring(game:GetObjects('rbxassetid://5671578245')[1].Source)() --// Main Script
