a3457926ec626c3d28312a7c268eb54e77ffbfcc
Alvin Lodel <73744009+alvin677@users.noreply.github.com>
2022-07-24 18:22:49 +0200

    Update dexv4.lua

--- dexv4.lua
+++ dexv4.lua
@@ -1,1 +1,2 @@
-loadstring(game:GetObjects('rbxassetid://418957341')[1].Source)()
+--loadstring(game:GetObjects('rbxassetid://418957341')[1].Source)()
+loadstring(game:HttpGet("https://paste.ee/r/DnETk"))()
